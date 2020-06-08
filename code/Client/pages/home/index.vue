fad0c21f50dd58cefd33a68062691b40e44fd3f7
GaochengLee <460270786@qq.com>
2020-06-08 10:00:45 +0800

    Update index.vue
    移动目录

--- code/Client/pages/home/index.vue
+++ code/Client/pages/home/index.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
-        <my-title style="width: 100%"></my-title>
+        <my-title style="width: 100%"/>
         <div class="bodyDiv">
-
+            
         </div>
-        <my-footer style="bottom: 0"></my-footer>
+        <my-footer style="bottom: 0"/>
     </div>
 </template>
 
 <script>
-    import MyTitle from "../components/myTitle";
-    import MyFooter from "../components/myFooter";
+    import MyTitle from "../../components/myTitle";
+    import MyFooter from "../../components/myFooter";
     export default {
         name: "firstPage",
         components: {MyFooter, MyTitle}
