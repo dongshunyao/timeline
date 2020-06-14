1f6dc5199efa767b0e611ae0285fc68aa51064af
PJYGit <870096160@qq.com>
2020-06-14 17:01:42 +0800

    task/add 修改sql键值

--- code/server/timeline/src/main/java/com/bjtu/timeline/controller/TaskController.java
+++ code/server/timeline/src/main/java/com/bjtu/timeline/controller/TaskController.java
@@ -1,5 +1,8 @@
 package com.bjtu.timeline.controller;
 
+import com.bjtu.timeline.bean.require.TaskRequires.*;
+import com.bjtu.timeline.bean.response.TaskResponses.*;
+import com.bjtu.timeline.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,4 +13,14 @@ import static com.bjtu.timeline.bean.response.CommonResponses.STATE_COMMON_OK;
 @RestController
 @RequestMapping("/task")
 public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @RequestMapping("/add")
+    public AddResponse add(AddRequire req) {
+        if (!taskService.checkUser(req)) {
+            return new AddResponse(STATE_COMMON_FAIL);
+        }
+        return taskService.addTask(req);
+    }
 }
--- code/server/timeline/src/main/java/com/bjtu/timeline/mapper/TaskMapper.java
+++ code/server/timeline/src/main/java/com/bjtu/timeline/mapper/TaskMapper.java
@@ -1,4 +1,19 @@
 package com.bjtu.timeline.mapper;
 
-public class TaskMapper {
+import com.bjtu.timeline.bean.require.TaskRequires;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Component;
+
+@Mapper
+@Component
+public interface TaskMapper {
+
+    @Select("select count(*) != 0 from user_login " +
+            "where uid = #{uid} and token = #{token}")
+    boolean checkToken(int uid, String token);
+
+    @Insert("insert into task (uid,gid,title,begin,end,detail,type) " +
+            "values (#{uid},#{groupid},#{title},#{begin},#{end},#{detail},#{type})")
+    void addTaskInfo(TaskRequires.AddRequire addRequire);
 }
