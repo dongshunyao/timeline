2067fd551ba9a1551da11c7cde120694e8d71390
wj <398636806@qq.com>
2020-06-16 03:01:25 +0800

    footer fix

--- code/Client/pages/setting.vue
+++ code/Client/pages/setting.vue
@@ -1,34 +1,40 @@
 <template>
-    <div>
-        <my-title/>
-        <div style="margin: 20px auto; width: 50%;">
-            <el-card>
-                <div style="text-align: center; margin: 10px;">
-                    <span style="font-size: 2rem;">
-                        个人设置
-                    </span>
-                </div>
-                <el-form label-width="100px" label-position="left">
-                    <el-form-item label="昵称">
-                        <el-input v-model="userName" clearable></el-input>
-                    </el-form-item>
-                    <el-form-item label="是否为会员">
-                        <el-input v-model="isVIP" disabled></el-input>
-                    </el-form-item>
-                    <el-form-item label="注册时间">
-                        <el-input v-model="registerTime" disabled></el-input>
-                    </el-form-item>
-                    <el-form-item label="密码修改">
-                        <el-input v-model="password" type="password" disabled clearable></el-input>
-                    </el-form-item>
-                </el-form>
-                <div style="text-align: center;">
-                    <el-button @click="updateUserInfo" type="primary" size="medium">确认更新</el-button>
-                    <el-button @click="resetUserInfo" size="medium">重置</el-button>
-                </div>
-            </el-card>
+    <div class="con">
+        <div class="wrap">
+            <div class="main">
+                <my-title/>
+                <div style="margin: 20px auto; width: 50%;">
+                <el-card>
+                    <div style="text-align: center; margin: 10px;">
+                        <span style="font-size: 2rem;">
+                            个人设置
+                        </span>
+                    </div>
+                    <el-form label-width="100px" label-position="left">
+                        <el-form-item label="昵称">
+                            <el-input v-model="userName" clearable></el-input>
+                        </el-form-item>
+                        <el-form-item label="是否为会员">
+                            <el-input v-model="isVIP" disabled></el-input>
+                        </el-form-item>
+                        <el-form-item label="注册时间">
+                            <el-input v-model="registerTime" disabled></el-input>
+                        </el-form-item>
+                        <el-form-item label="密码修改">
+                            <el-input v-model="password" type="password" disabled clearable></el-input>
+                        </el-form-item>
+                    </el-form>
+                    <div style="text-align: center;">
+                        <el-button @click="updateUserInfo" type="primary" size="medium">确认更新</el-button>
+                        <el-button @click="resetUserInfo" size="medium">重置</el-button>
+                    </div>
+                </el-card>
+            </div>
+            </div>
+            <div class="footer">
+                <my-footer/>
+            </div>
         </div>
-        <my-footer/>
     </div>
 </template>
 
@@ -104,5 +110,47 @@
 </script>
 
 <style scoped>
+    .con{
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        top:0;bottom: 0;left: 0;right: 0px
+    }
+    .wrap{
+        
+        min-height: 100%;
+        width: 100%;
+        position: relative;
+        
+        display:-moz-box; 
+        display:-webkit-box; 
+        display:-o-box; 
+        display:box;
 
+        -moz-box-orient:vertical; 
+        -webkit-box-orient:vertical; 
+        -o-box-orient:vertical; 
+        box-orient:vertical; 
+
+        -moz-box-align:center; 
+        -webkit-box-align:center; 
+        -o-box-align:center; 
+        box-align:center; 
+
+        -moz-box-pack:center; 
+        -webkit-box-pack:center; 
+        -o-box-pack:center; 
+        box-pack:center;      
+    }
+    .main{
+        width: 100%;
+         -moz-box-flex: 1; 
+        -webkit-box-flex: 1; 
+        box-flex: 1;
+        background-color: #efefef;
+    }
+    .footer{
+        width:100%;
+        height: 3rem;
+    }
 </style>
